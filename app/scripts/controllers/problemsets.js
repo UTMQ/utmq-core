90bc2c355eff04b401fa43a1528a93466bed2f79
vladikoff <vlad.filippov@gmail.com>
2013-10-04 02:46:48 -0400

    Fix bootstrap paths & js issues, add a few buttons

--- app/scripts/controllers/problemsets.js
+++ app/scripts/controllers/problemsets.js
@@ -3,8 +3,14 @@
 angular.module('UTMQViewerApp')
   .controller('ProblemSetCtrl', function($scope) {
     $scope.problemSets = [
-      {name: 'Greedy'},
-      {name: 'Divide and Conquer'},
-      {name: 'Dynamic Programming'},
+      {name: 'Greedy', date: 'Septermber 20 2013 at 2:51pm'},
+      {name: 'Divide and Conquer', date: 'September 28 2013 at 3:12am'},
+      {name: 'Dynamic Programming', date: 'October 1 2013 at 6:23pm'}
+    ];
+    $scope.actions = [
+      {action: 'Publish'},
+      {action: 'Clone'},
+      {action: 'Download'},
+      {action: 'Delete'}
     ];
   });
