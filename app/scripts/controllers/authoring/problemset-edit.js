65f209f61cb43df3587e9d9e46857834d76b2fae
vladikoff <vlad.filippov@gmail.com>
2013-11-20 21:58:20 -0500

    Removed pouch, added lodash

--- app/scripts/controllers/authoring/problemset-edit.js
+++ app/scripts/controllers/authoring/problemset-edit.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('UTMQViewerApp')
-  .controller('AuthoringProblemSetEditCtrl', function($scope, pouchService, FormService) {
+  .controller('AuthoringProblemSetEditCtrl', function($scope, FormService) {
 
 
     // preview form mode
